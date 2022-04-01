9cd2dc87ad69ce36c42cedc01693ea322bfcd49d
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-04-01 10:17:11 -0300

    Update TitleState.hx

--- Ghost engine/source/TitleState.hx
+++ Ghost engine/source/TitleState.hx
@@ -20,7 +20,6 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
-import io.newgrounds.NG;
 import lime.app.Application;
 import openfl.Assets;
 
@@ -50,14 +49,6 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
-		#if polymod
-		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
-		#end
-		
-		#if sys
-		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
-			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
-		#end
 
 		
 		PlayerSettings.init();
